278eee2c4ad853fd184167263e6b9e7e7c639c0a
yangxia605 <yangxia605@gmail.com>
2020-07-27 16:51:23 +0800

    '对象'

---
+++ study/src/java_basic/basic/objectoriented/Phone.java
@@ -0,0 +1,13 @@
+package java_basic.basic.objectoriented;
+
+/**
+ * 定义手机类
+ * 类名：Phone
+ * 成员变量：String brand 品牌；String model 型号；String name 名称
+ * 成员方法：去掉 static修饰符 call()  打电话；sendMessage（） 发短信//
+ * 使用：
+ */
+
+public class Phone {
+
+}
