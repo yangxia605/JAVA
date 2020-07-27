40acf8e764d974a1274fd6294e6961ee295fabd6
yangxia605 <yangxia605@gmail.com>
2020-07-27 13:53:54 +0800

    'java基础知识'

---
+++ study/src/java_basic/basic/HelloWorld.java
@@ -0,0 +1,11 @@
+package java_basic.basic;
+
+/***
+ * Java程序的入口是main方法，main方法的格式是固定的
+ */
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
